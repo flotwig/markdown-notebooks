9cb20e297c954db41af010028199b8032e51cd71
Zach Bloomquist <zbloomq@live.com>
2019-01-05 11:31:17 -0500

    Merge PageEditor and NotebookEditor

--- src/components/notebook/NotebookEditor.js
+++ src/components/notebook/NotebookEditor.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import PageEditor from './PageEditor';
+import MarkdownEditor from '../markdown/MarkdownEditor';
+import MarkdownRenderer from '../markdown/MarkdownRenderer';
 import ConnectedOpenMenu from './OpenMenu';
 import PageList from './PageList';
-import { Icon, Button, ButtonGroup, Divider, Dialog, NonIdealState, Spinner } from '@blueprintjs/core';
+import { 
+    Icon, Button, ButtonGroup, Divider, Dialog, 
+    NonIdealState, Spinner, H2, EditableText, Card
+} from '@blueprintjs/core';
 
 export default class NotebookEditor extends React.Component {
     constructor(props) {
         super(props)
+        const activePage = this.getActivePage()
         this.state = {
-            isOpenMenuOpen: false
+            isOpenMenuOpen: false,
+            activePageContent: activePage.content,
+            activePageName: activePage.name
         }
         this.onClickSave = this.onClickSave.bind(this)
         this.onClickOpen = this.onClickOpen.bind(this)
         this.handleEdit = this.handleEdit.bind(this)
+        this.handleNameChange = this.handleNameChange.bind(this)
+        this.handlePaste = this.handlePaste.bind(this)
+        this.textareaRef = React.createRef();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.activePageId !== this.props.activePageId) {
+            const activePage = this.getActivePage()
+            this.setState({
+                activePageContent: activePage.content,
+                activePageName: activePage.name
+            })
+        }
     }
 
     render() {
@@ -32,12 +52,12 @@ export default class NotebookEditor extends React.Component {
                             <Button text="Save" icon="cloud-upload" onClick={this.onClickSave}/>
                             <Button text="Open" icon="cloud-download" onClick={this.onClickOpen}/>
                             <Divider/>
-                            <Button text="New Page" onClick={()=>this.props.addPage()}/>
-                            <Button text="Delete Page" onClick={()=>this.props.deletePage()}/>
+                            <Button text="New Page" onClick={this.props.addPage}/>
+                            <Button text="Delete Page" onClick={this.props.deletePage}/>
                             <Divider/>
                         </ButtonGroup>
                         {this.props.notebook && <PageList pages={this.props.notebook.pages} 
-                                                          activePage={this.props.notebook.pages.find(p => p._id ===this.props.activePageId)} 
+                                                          activePage={this.getActivePage()} 
                                                           onClickPage={this.props.setActivePage}/>
                         }
                     </div>
@@ -57,23 +77,64 @@ export default class NotebookEditor extends React.Component {
 
     renderEditor() {
         return (
-                <PageEditor page={this.props.notebook.pages.find(p => p._id === this.props.activePageId)}
-                            handleEdit={this.handleEdit}
-                            uploadImage={this.props.uploadImage}/> 
+            <React.Fragment>
+                <div style={{width: '50%', flex: 'auto', display: 'flex', flexDirection: 'column', marginRight: '1%'}}>
+                    <div style={{maxWidth: '100%', overflow: 'cli+p'}}>
+                        <H2>
+                            <EditableText value={this.state.activePageName} 
+                                        placeholder="Untitled Page" 
+                                        onChange={(activePageName)=>this.setState({ activePageName })}
+                                        onConfirm={this.handleNameChange}/>
+                        </H2>
+                    </div>
+                    <MarkdownEditor markdown={this.state.activePageContent} 
+                                    onChange={this.handleEdit}
+                                    textareaRef={this.textareaRef}
+                                    onPaste={this.handlePaste}/>
+                </div>
+                <Card style={{width: '40%', height: '100%', overflow: 'auto'}}>
+                    <MarkdownRenderer markdown={this.state.content}/>
+                </Card>
+            </React.Fragment>
         )
+    }
+
+    getActivePage() {
+        return this.props.notebook.pages.find(p => p._id === this.props.activePageId);
     }
 
     onClickSave() {
         this.props.onClickSave && this.props.onClickSave(this.props.notebook)
     }
 
-    handleEdit(page) {
-        this.props.handleEdit && this.props.handleEdit(page)
+    handleEdit(content) {
+        this.setState({ activePageContent: content })
+        this.props.handleEdit && this.props.handleEdit({ name: this.state.name, content })
+    }
+
+    handleNameChange(name) {
+        this.setState({ activePageName: name })
+        this.props.handleEdit && this.props.handleEdit({ name, content: this.state.content })
     }
 
     onClickOpen() {
         this.setState({
             isOpenMenuOpen: true
         })
+    }
+
+    handlePaste(e) {
+        // cancel default behavior and stop bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        let items = e.clipboardData.items
+        for (var i = 0; i < items.length; i++) {
+            let item = items[i]
+            if (item.type.includes('image')) {
+                const cursorLocation = this.textareaRef.current.selectionStart
+                const blob = item.getAsFile()
+                this.props.uploadImage(blob, cursorLocation)
+            }
+        }
     }
 }
