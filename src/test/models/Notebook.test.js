7c2ee704a25dbed1e9227dc010600064b9c0e4db
Zach Bloomquist <zbloomq@live.com>
2019-01-07 11:21:42 -0500

    Adding tests for withChanges

--- src/test/models/Notebook.test.js
+++ src/test/models/Notebook.test.js
@@ -1,10 +1,6 @@
 import Notebook from '../../models/Notebook';
 import NotebookPage from '../../models/NotebookPage';
 
-const mockGist = {
-
-}
-
 it('creates a notebook with one page by default', () => {
     const notebook = new Notebook();
     expect(notebook.pages).toHaveLength(1);
@@ -17,4 +13,16 @@ it('returns sequential untitled page titles', () => {
     expect(notebook.pages[0].name).toEqual('Untitled Page');
     expect(notebook.pages[1].name).toEqual('Untitled Page (1)');
     expect(notebook.pages[2].name).toEqual('Untitled Page (2)');
+})
+
+it('does not set the modified flag when no changes have been made', () => {
+    const notebook = new Notebook();
+    const changed = notebook.withChanges({});
+    expect(changed.modified).toEqual(false);
+})
+
+it('does set the modified flag when changes have been made', () => {
+    const notebook = new Notebook();
+    const changed = notebook.withChanges({ name: 'New Name' });
+    expect(changed.modified).toEqual(true);
 })
