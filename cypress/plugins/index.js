b78606e5e2ad5565ef15fdac560d33b6b516109d
Zach Bloomquist <github@chary.us>
2019-02-12 10:23:30 -0500

    fix pluginsfile

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-require('cypress-get-react-component').default.install()
+//require('cypress-get-react-component').default.install()
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
