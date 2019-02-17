bc68b3357cbbbb65f32b2474c4c9ee0cd04eb6c7
Zach Bloomquist <github@chary.us>
2019-02-17 17:34:24 -0500

    wtf?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
 import ConnectedGitHubAuth from './components/auth/GitHubAuth'
 import ConnectedNotebookEditor from './components/notebook/ConnectedNotebookEditor'
 
@@ -9,12 +8,10 @@ import ConnectedNotebookEditor from './components/notebook/ConnectedNotebookEdit
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
         <>
           <ConnectedGitHubAuth/>
           <ConnectedNotebookEditor/>
         </>
-      </BrowserRouter>
     );
   }
 }
